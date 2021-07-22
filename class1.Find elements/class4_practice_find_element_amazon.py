894ce460d7ced75afd938e2d5a92da07c2f626f6
Barracudakun <yingxukun@yahoo.com>
2021-07-21 21:50:39 -0500

    update

--- class1.Find elements/class4_practice_find_element_amazon.py
+++ class1.Find elements/class4_practice_find_element_amazon.py
@@ -1,16 +1,10 @@
-# -*- coding = utf-8 -*-
-# @Time: 6/12/21 1:59 PM
-# @Author: YAO
-# @File: class4_practice_find_element_amazon.py
-# @Software: PyCharm
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-driver = webdriver.Chrome('/Users/seashore/Dropbox/My Mac (MacBook-Air.lan)/Desktop/chromedriver')
+driver = webdriver.Chrome('/Users/seashore/PycharmProjects/pythonProject/chromedriver')
 driver.get('http://www.amazon.com')
 driver.find_element_by_id('twotabsearchtextbox').send_keys('shoes')
 sleep(2)
-driver.find_element_by_id('nav-search-submit-button').click()
+driver.find_element(By.ID, 'nav-search-submit-button').click()
 sleep(2)
 driver.quit()
--- features/steps/behave.py
+++
@@ -1,9 +0,0 @@
-# -*- coding = utf-8 -*-
-# @Time: 6/26/21 4:26 PM
-# @Author: YAO
-# @File: behave.py
-# @Software: PyCharm
-
-from behave import *
-from selenium import webdriver
-
